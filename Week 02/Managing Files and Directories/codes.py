7c0d4192b193de7b38da50dd8424df5fdc899eea
Muntaha Islam <muntaha.islam0019@gmail.com>
2021-02-16 00:16:59 +0600

    Changes in Managing Files and Directories

--- Week 02/Managing Files and Directories/codes.py
+++ Week 02/Managing Files and Directories/codes.py
@@ -23,3 +23,36 @@ print(os.path.abspath('spider.txt'))
 
 # Current working directory
 print(os.getcwd())
+
+# Make a new directory
+# os.mkdir('new_directory')
+
+# Change the directory
+os.chdir('new_directory')
+print(os.getcwd())
+
+# Remove a directory
+os.mkdir('newer_directory')
+os.rmdir('newer_directory')
+# One need to delete all the files in a directory before to remove the directory
+# A directory which consists files, will throw an error if someone tries to remove it
+
+# Listing files in a directory
+# os.mkdir('a_dir')
+# os.mkdir('another_dir')
+os.chdir('..')
+# Goes to parent directory
+print(os.listdir('new_directory'))
+
+# Joining path names & checking if its a directory
+os.chdir('new_directory')
+with open('a_text.txt', 'w') as file:
+    file.write("Well, it's a line")
+os.chdir('..')
+dir_name = 'new_directory'
+for name in os.listdir(dir_name):
+    full_path = os.path.join(dir_name, name)
+    if os.path.isdir(full_path):
+        print('{} is a directory'.format(full_path))
+    else:
+        print('{} is a file'.format(full_path))
