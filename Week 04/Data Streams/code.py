06bb417c6ecc0fcf9ee8195969053a96e64b6d52
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-04-12 10:30:30 +0600

    Data Streams

--- Week 04/Data Streams/code.py
+++ Week 04/Data Streams/code.py
@@ -2,5 +2,5 @@ import os
 
 # To access environment variables:
 print("TEMP: " + os.environ.get("TEMP", ""))
-# If a variable isn't present, the above line would print a black space. To export a new variable, write export VAR_NAME = VALUE in command line
+# If a variable isn't present, the above line would print a black space. To export a new variable, write "export VAR_NAME = VALUE" in command line
 
