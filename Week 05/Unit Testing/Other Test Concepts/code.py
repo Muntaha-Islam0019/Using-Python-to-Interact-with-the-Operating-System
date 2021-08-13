e70ad7cd61bd616dcbcaad7a31669ad2d78accac
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-08-13 22:00:47 +0600

    Other Test Concepts

---
+++ Week 05/Unit Testing/Other Test Concepts/code.py
@@ -0,0 +1,13 @@
+# White-box testing also sometimes called clear-box or transparent testing relies on the test creators knowledge of the software being tested to construct the test cases. With a white-box test, the test creator knows how the code works and can write test cases that use the understanding to make sure that everything is performing the way it's expected to
+
+# In black-box testing, the software being tested is treated like an opaque box. In other words, the tester doesn't know the internals of how the software works. Black-box tests are written with an awareness of what the program is supposed to do, its requirements or specifications, but not how it does it
+
+# Unit test allows the test to verify the unit provides expected functionality regardless of the rest of the environment
+
+# Integration tests verify that the interactions between the different pieces of code in integrated environments are working the way we expect them to. They usually take the individual modules of code that unit test verify then combine them into a group to test
+
+# A variant of unit tests our regression tests. They're usually written as part of a debugging and troubleshooting process to verify that an issue or error has been fixed once it's been identified
+
+# Smoke test answer basic questions like, does the program run
+
+# Load tests verify that the system behaves well when it's under significant load
